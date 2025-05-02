494141da331ca4a4890356c7319cab470d079be5
Lucas Azevedo <lucas.shirabayashi@gmail.com>
2025-05-02 21:21:36 +0100

    feat(home): add Home page layout and styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,18 @@
+import styles from "./Home.module.scss"
+import HomeImg from "../assets/home.png"
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world</h1>
-      <p>Welcome to my website!</p>
+    <div className={styles.container}>
+
+      <div className={styles.message}>
+        <h2>Find clothes that matches your style</h2>
+        <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+        <button>Shop Now</button>
+      </div>
+
+      <img src={HomeImg} alt="" />
+
     </div>
   )
 }
