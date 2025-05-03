9afc66da16c1e74f86601d53888af14237a20d43
Lucas Azevedo <lucas.shirabayashi@gmail.com>
2025-05-03 17:00:32 +0100

    feat: finish Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,6 +9,23 @@ export default function Home() {
         <h2>Find clothes that matches your style</h2>
         <p>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
         <button>Shop Now</button>
+
+        <div className={styles.stats}>
+          <div>
+            <h3>200+</h3>
+            <p>International Brands</p>
+          </div>
+
+          <div>
+            <h3>2,000+</h3>
+            <p>High-Quality Products</p>
+          </div>
+
+          <div>
+            <h3>30,000+</h3>
+            <p>Happy Customers</p>
+          </div>
+        </div>
       </div>
 
       <img src={HomeImg} alt="" />
